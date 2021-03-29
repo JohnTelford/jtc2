4d9f03762e888b0650b6beb0dc1776f78e0fa5da
John <john@johntelford.com>
2021-03-28 17:01:17 -0700

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,85 @@
+const path = require('path')
+
+module.exports = {
+  siteMetadata: {
+    // title: 'Gatsby Starter MDX Basic',
+    title : 'JohnTelford.com LLC',
+    description:
+      'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
+    author: '@chrisbiscardi',
+      menuLinks:[
+        {
+          name : 'home',
+          link : '/'
+        },
+
+        {
+          name : 'about',
+          link : '/about'
+        },
+      ]
+  },
+  plugins: [
+    {
+      resolve: '@stackbit/gatsby-plugin-menus',
+      options: {
+        // static definition of menu items (optional)
+        menus: {
+          main: // identifier of menu container
+            [ // array of contained children menu items
+              {
+                identifier: 'myId', // identifier for this item (optional)
+                title: 'Title for page',
+                url: '/page-1/',
+                weight: 1
+              }
+            ]
+        },
+        // Gatsby node types from which we extract menus (optional, see "Advanced usage")
+        sourceNodeType: 'MarkdownRemark', 
+        // the relative node path where we can find the 'menus' container (optional)
+        sourceDataPath: 'frontmatter',
+        // the relative node path where we can find the page's URL (required)
+        sourceUrlPath: 'fields.url',
+        // custom menu loading function (optional)
+        // menuLoader: customLoaderFunction,
+        // the property to use for injecting to the page context (optional, see "Advanced usage")
+        pageContextProperty: 'menus', 
+      },
+    },
+
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: { default: path.resolve('./src/components/layout.js') },
+      },
+    },
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-default-mdx-basic',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    // 'gatsby-plugin-offline',
+  ],
+
+}
