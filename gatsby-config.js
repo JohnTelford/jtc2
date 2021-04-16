00eb35398b122c57b04545593a8f296eb1d5b510
John <john@johntelford.com>
2021-04-16 16:09:27 -0700

    Apr 16, 2021 04-08 PM
    Signed-off-by: John <john@johntelford.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,22 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+
+    {
       resolve: '@stackbit/gatsby-plugin-menus',
       options: {
         // static definition of menu items (optional)
